102c0d6222fe36327d99db1fda75b1a1413546bb
Razvan Negrea <rznart@gmail.com>
2021-05-12 12:46:30 +0200

    Move the boid colours to its own constant file

--- src/scripts/config.ts
+++ src/scripts/config.ts
@@ -1,18 +1,5 @@
-const COLORS = [
-  "#f89a99",
-  "#FD4365",
-  "#ee6048",
-  "#f89859",
-  "#f8eac0",
-  "#f8ca65",
-  "#f8ba2d",
-  "#f3ca85",
-  "#c5eb9d",
-  "#a4d5a3",
-  "#bceae9",
-  "#95d3d5",
-  "#c2a4e7",
-]
+// Constants
+import { BOID_COLORS } from "./constants/colors"
 
 export const config = {
   boids: {
@@ -21,6 +8,6 @@ export const config = {
     maxSize: 60,
     brakingFactor: 15,
     awarenessFactor: 10,
-    colors: COLORS,
+    colors: BOID_COLORS,
   },
 }

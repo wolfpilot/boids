14950d9dbf2fbb1bf3bfb5272d1b64b6ac77abd2
Razvan Negrea <rznart@gmail.com>
2021-05-16 14:08:20 +0200

    Fix boidHelper test not setting the Canvas context correctly

--- src/scripts/utils/boidHelper.spec.ts
+++ src/scripts/utils/boidHelper.spec.ts
@@ -9,13 +9,12 @@ import { BOID_COLORS } from "../constants/colors"
 // Actors
 import Boid from "../actors/Boid/Boid"
 
-// let windowSpy
-let canvas
-let ctx
+let canvas: HTMLCanvasElement
+let ctx: CanvasRenderingContext2D
+let randomBoids: Boid[] = []
 
 const options = {
   total: 5,
-  ctx,
   maxX: 1920,
   maxY: 1080,
   minSize: 10,
@@ -28,27 +27,37 @@ const options = {
   colors: BOID_COLORS,
 }
 
-const randomBoids = generateRandomizedBoids(options)
-
 beforeEach(() => {
   canvas = document.createElement("canvas")
   ctx = canvas.getContext("2d")
+
+  randomBoids = generateRandomizedBoids({
+    ...options,
+    ctx,
+  })
 })
 
 describe("actorHelper", () => {
   it("should generate an array of Boids within the specified constraints", () => {
     expect(randomBoids.length).toBe(options.total)
-  })
 
-  test.each(randomBoids)("should validate each Boid instance", (boid) => {
-    expect(boid instanceof Boid).toBe(true)
+    /**
+     * Use JS forEach loop as test.each() does not work
+     * with dynamically-generated mock data.
+     *
+     * For more info, see:
+     * https://github.com/facebook/jest/issues/6888
+     */
+    randomBoids.forEach((boid) => {
+      expect(boid instanceof Boid).toBe(true)
 
-    expect(options.colors).toContain(boid.traits.color)
-    expect(boid.traits.size).toBeGreaterThanOrEqual(options.minSize)
-    expect(boid.traits.size).toBeLessThanOrEqual(options.maxSize)
+      expect(options.colors).toContain(boid.traits.color)
+      expect(boid.traits.size).toBeGreaterThanOrEqual(options.minSize)
+      expect(boid.traits.size).toBeLessThanOrEqual(options.maxSize)
 
-    const spyInit = jest.spyOn(Boid.prototype, "init")
-    boid.init()
-    expect(spyInit).toHaveBeenCalled()
+      const spyInit = jest.spyOn(Boid.prototype, "init")
+      boid.init()
+      expect(spyInit).toHaveBeenCalled()
+    })
   })
 })
